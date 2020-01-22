dd6c6b0be6c2257cb5c6318948e02c35d3476e56
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-01-22 12:05:22 -0800

    another successful installation on laptop

--- lib/install/awesome.sh
+++ lib/install/awesome.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yay -S --noconfirm --needed fortune-mod awmtt feh networkmanager-dmenu-git lain rlwrap dex xcb-util-errors awesome-freedesktop vicious 
+yay -S --noconfirm --needed fortune-mod awmtt feh networkmanager-dmenu-git lain rlwrap dex xcb-util-errors awesome-freedesktop vicious  hddtemp wireless_tools 
