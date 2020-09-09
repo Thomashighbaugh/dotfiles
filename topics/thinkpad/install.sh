042e2600bfa677b2760f6a6228727dea2aafb3f1
Thomas Leon HIghbaugh <thighbaugh@zoho.com>
2020-09-08 22:55:52 -0700

    makefile group update

---
+++ topics/thinkpad/install.sh
@@ -0,0 +1,1 @@
+yay -S --noconfirm --sudoloop --needed tp_smapi thinkfan thinkalert tp-battery-mode thinkpad-scripts thinkpad_wmi-dkms tpfand-git
---
+++ topics/tlp/install.sh
@@ -0,0 +1,4 @@
+yay -S --noconfirm --sudoloop --needed tlp powertop acpid tlp-rdw system76-power
+
+sudo systemctl enable --now tlp 
+sudo systemctl enable --now system76-power
