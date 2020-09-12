695215dd49ba411197fb90a17dbb4cbceb5bdb64
Thomas Leon HIghbaugh <thighbaugh@zoho.com>
2020-09-11 21:49:07 -0700

    makefile group update

--- topics/thinkpad/install.sh
+++ topics/thinkpad/install.sh
@@ -1,1 +1,7 @@
+#!/bin/bash 
+
+################################################################################
+## Thinkpad Installer ##########################################################
+################################################################################
+
 yay -S --noconfirm --sudoloop --needed tp_smapi thinkfan thinkalert tp-battery-mode thinkpad-scripts thinkpad_wmi-dkms tpfand-git
