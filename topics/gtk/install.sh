e29c1d866ff05bcc483b058f57f3146f4783f687
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-10-06 08:02:25 +0530

    touch ups

--- topics/gtk/install.sh
+++ topics/gtk/install.sh
@@ -5,7 +5,6 @@
 ################################################################################
 yay -S --noconfirm numix-gtk-theme  arc-gtk-theme  gtk-engine-murrine gtk-engines 
 
-
 mkdir -p $HOME/.config/gtk-2.0
 
 ln -svf $HOME/dotfiles/topics/gtk/gtk-2.0/gtkfilechooser.ini $HOME/.config/gtk-2.0/gtkfilechooser.ini
@@ -23,5 +22,7 @@ ln -svf $HOME/dotfiles/topics/gtk/gtkrc-2.0 $HOME/.gtkrc-2.0
 mkdir -p $HOME/.config/Kvantum
 
 ln -svf $HOME/dotfiles/topics/gtk/kvantum.kvconfig $HOME/.config/Kvantum/kvantum.kvconfig
+
+sudo git clone https://github.com/Thomashighbaugh/dhumavati /usr/share/themes/dhumavati
 
 
