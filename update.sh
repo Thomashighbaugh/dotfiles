e160b10911ef0f01790b5f564bde0739e2e66387
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-10-08 05:28:57 -0700

    dotfiles group update

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@
    
     cd ${HOME}/.local/share/bin && git add . && git commit -m "updated" && git push --force
 
-    cd ${PWD} && git add . && git commit -m "dotfiles group update" && git push --force
+    cd ${HOME}/dotfiles && git add . && git commit -m "dotfiles group update" && git push --force
