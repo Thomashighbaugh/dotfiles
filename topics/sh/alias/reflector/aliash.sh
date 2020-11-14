0b8073420c722e934219118eecf2ff34319ed0e0
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-11-14 17:56:46 +0530

    Committed by tlh

--- topics/sh/alias/reflector/aliash.sh
+++ topics/sh/alias/reflector/aliash.sh
@@ -4,4 +4,4 @@
 
 # Reflector
 ######################################################################################
-alias mirrors="sudo reflector --verbose --country 'United States' -l 0 --sort rate --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --verbose --country 'United States'  --sort rate --save /etc/pacman.d/mirrorlist"
