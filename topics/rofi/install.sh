29a4b0d87a823bd18d0ebb822208bfc2a36ef77c
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-11-26 07:36:20 +0530

    makefile group update

--- topics/rofi/install.sh
+++ topics/rofi/install.sh
@@ -4,10 +4,11 @@
 ## bash installer ##############################################################
 ################################################################################
 
-yay -S  --noconfirm  --sudoloop  --needed  rofi-git
+yay -S  --noconfirm  --sudoloop  --needed  rofi-git rofi-todo
 
 mkdir -p  $HOME/.config/rofi 
-ln -svf   $HOME/dotfiles/topics/rofi/config       $HOME/.config/rofi/config
-ln -svf   $HOME/dotfiles/topics/rofi/themes       $HOME/.config/rofi/themes
+ln -svf   $HOME/dotfiles/topics/rofi/config       $HOME/.config/rofi
+ln -svf   $HOME/dotfiles/topics/rofi/themes       $HOME/.config/rofi
+sudo ln -svf   $HOME/dotfiles/topics/rofi/themes  /usr/share/rofi
 ln -svf   $HOME/dotfiles/topics/rofi/config.rasi  $HOME/.config/rofi/config.rasi
 ln -svf   $HOME/dotfiles/topics/rofi/three.rasi   $HOME/.config/rofi/three.rasi
