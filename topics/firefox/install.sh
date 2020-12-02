21f1d188f784c9549c6d8b590d570568b299ba34
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-02 16:51:15 +0530

    makefile group update

--- topics/firefox/install.sh
+++ topics/firefox/install.sh
@@ -1,6 +1,8 @@
+#!/bin/env bash
+
 sudo pacman -S --noconfirm firefox 
 
 yay -S --noconfirm --sudoloop tor-browser 
 
-git clone https://github.com/Thomashighbaugh/firefox ~/.local/share/firefox/chrome 
+git clone https://github.com/Thomashighbaugh/firefox $HOME/.local/share/firefox/chrome 
 
---
+++ topics/gnupg/install.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+sudo pacman -S --noconfirm gnupg gmime3 libcryptui seahorse mcabber hopenpgp-tools 
