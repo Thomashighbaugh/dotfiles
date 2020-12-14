cf51da21b2f686e475b5ff914761bf52843b86f9
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-15 01:58:40 +0530

    dotfiles group update

--- topics/icons/install.sh
+++ topics/icons/install.sh
@@ -1,22 +1,4 @@
 #!/bin/env bash
 
 # Paper
-yay -S --noconfirm paper-icon-theme-git
 
-# superflat remix icons
-yay -S --noconfirm oranchelo-icon-theme
-git clone https://github.com/daniruiz/Super-Flat-Remix.git /tmp/Super-Flat-Remix
-mkdir ~/.icons/Super-Flat-Remix
-cp -rvf /tmp/Super-Flat-Remix/Super\ Flat\ Remix/* ~/.icons/Super-Flat-Remix
-sudo cp -rvf /tmp/Super-Flat-Remix/Super\ Flat\ Remix/* /usr/share/icons
-
-#Flatwoken
-yay -S --noconfirm awoken-icon-theme
-yay -S --noconfirm flatwoken-icons-git
-
-# Moka and Faba
-yay -S --noconfirm moka-icon-theme-git
-yay -S --noconfirm faba-icon-theme-git
-
-# Papirus
-yay -S --noconfirm papirus-icon-theme-gtk
---
+++ topics/torrents/install.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+yay -S --noconfirm --needed --sudoloop transmission-gtk transmission-cli libtorrent
