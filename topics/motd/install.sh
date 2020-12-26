0e7ac05179caecea9b86aeb452a9adb6af4d4cd2
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-26 13:07:18 -0800

    dotfiles group update

---
+++ topics/motd/install.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash 
+
+sudo ln -svf $HOME/dotfiles/topics/motd/motd /etc/motd
+sudo ln -vf $HOME/dotfiles/topics/motd/motd.sh /etc/motd.sh
---
+++ topics/sh/alias/mkarchiso.alias.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh
+
+# intended for use in building Electric Tantra Linux, but useful with mkarchiso in general. Especially as it uses the tmpfs to house the work product, a huge improvement in terms of speed and keeping directories well ordered and easily wiped clean.
+
+alias isobuild="sudo mkarchiso -v -w /tmp/mkarchiso-tmp ./releng"
+
+alias isoclean="mv out/*.iso ../ && sudo rm -rvf out && sudo rm -rvf /tmp/mkarchiso-tmp && echo 'ISO build has been cleaned up... hopefully'" 
