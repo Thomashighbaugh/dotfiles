ce4125fa87c685394a4edb416a62463e6a4a81b5
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-31 00:22:23 +0530

    dotfiles group update

--- topics/git/install.sh
+++ topics/git/install.sh
@@ -5,3 +5,6 @@
 ##############################################################################
 
 sudo pacman -S --noconfirm git
+
+# Enter your git credentials once, forget it for the rest of the install
+git config --global credential.helper store
