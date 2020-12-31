68cab1eaaf3f832c5a2ad776fbceb05dac808e04
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2020-12-31 06:44:04 -0800

    dotfiles group update

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
 # #!/usr/bin/env bash
-echo ""
-echo "  _____         __    ___ __ __                   "
-echo " |     \.-----.|  |_.'  _|__|  |.-----.-----.     "
-echo " |  --  |  _  ||   _|   _|  |  ||  -__|__ --|     "
-echo " |_____/|_____||____|__| |__|__||_____|_____|     "
-echo "  __               __          __ __              "
-echo " |__|.-----.-----.|  |_.---.-.|  |  |.-----.----. "
-echo " |  ||     |__ --||   _|  _  ||  |  ||  -__|   _| "
-echo " |__||__|__|_____||____|___._||__|__||_____|__|   "
-echo ""
-echo ""
+
+echo -n "
+  _____         __    ___ __ __                   
+ |     \.-----.|  |_.'  _|__|  |.-----.-----.     
+ |  --  |  _  ||   _|   _|  |  ||  -__|__ --|     
+ |_____/|_____||____|__| |__|__||_____|_____|     
+  __               __          __ __              
+ |__|.-----.-----.|  |_.---.-.|  |  |.-----.----. 
+ |  ||     |__ --||   _|  _  ||  |  ||  -__|   _| 
+ |__||__|__|_____||____|___._||__|__||_____|__|   "
+echo
+echo
+echo
 echo "First let's make sure we get the basics out of the way."
 echo "Press any key to install the dependencies and set up your system:"
 read continue
@@ -23,11 +25,11 @@ deps() {
 	sudo pacman -S --noconfirm git pacutils fakeroot perl-libwww perl-term-ui perl-json perl-data-dump perl-lwp-protocol-https perl-term-readline-gnu
 
 	echo "Install Yay"
-	git clone https://aur.archlinux.org/yay.git $HOME/yay && cd $HOME/yay && makepkg -is && cd .. && sudo rm -rf $HOME/yay
+	git clone https://aur.archlinux.org/yay.git $HOME/yay && cd $HOME/yay && makepkg -s && cd .. && sudo rm -rf $HOME/yay
 
 	echo "Install some other packages useful for the rest of the installation process."
 
-	yay -S --needed --sudoloop --noconfirm chrony atool autoconf autocutsel automake binutils bison
+	yay -S --needed --sudoloop --noconfirm atool autoconf autocutsel automake binutils bison
 
 	yay -S --needed --sudoloop --noconfirm gcc gcc-libs gettext glibc dialog dmidecode
 
@@ -35,18 +37,102 @@ deps() {
 
 }
 
-laptop() {
+####################################################################################
+## Application Installation FUnctions ##############################################
+####################################################################################
+amd() {
+	yay -S --noconfirm --needed --sudoloop amd-ucode ryzenadj-git zenstates-git ryzen-stabilizator-git linux-amd-znver2-docs linux-amd-znver2-headers linux-amd-znver2
 
-	sh $HOME/dotfiles/topics/awesome/install.sh
-	sh $HOME/dotfiles/topics/bash/install.sh
-	sh $HOME/dotfiles/topics/bitwarden/install.sh
-	sh $HOME/dotfiles/topics/bluetooth/install.sh
-	sh $HOME/dotfiles/topics/bspwm/install.sh
-	sh $HOME/dotfiles/topics/chromium/install.sh
-	sh $HOME/dotfiles/topics/cups/install.sh
-	sh $HOME/dotfiles/topics/cursor/install.sh
-	sh $HOME/dotfiles/topics/disks/install.sh
-	sh $HOME/dotfiles/topics/docker/install.sh
+	sudo grub-mkconfig -o /boot/grub/grub.conf
+
+	sudo mkinitcpio -P
+}
+awesomewm() {
+	yay -S --noconfirm --sudoloop awesome-git feh xcape
+
+	yay -S --noconfirm --sudoloop vicious lain-git awesome-freedesktop-git archlinux-xdg-menu archlinux-menus
+
+	yay -S --noconfirm --sudoloop bemenu blueberry blueman bluez-hid2hci bluez-tools bluez-utils
+
+	yay -S --noconfirm --sudoloop awmtt thunar thunar-archive-plugin thunar-media-tags-plugin thunar-volman
+
+	yay -S --noconfirm --sudoloop maim firefox luarocks nitrogen
+
+	## Erase Current Configuration
+
+	if [[ -d "$HOME/.config/awesome" ]]; then
+		mv $HOME/.config/awesome $HOME/.config/aweesome.old
+	fi
+
+	## Install My Configuration
+	git clone https://github.com/Thomashighbaugh/awesomewm $HOME/.config/awesome
+}
+
+bash() {
+	## Install Packages
+	sudo pacman -S bash shellharden bashlint
+
+	## Link Files
+	ln -svf $HOME/dotfiles/topics/bash/bashenv $HOME/.bashenv
+	ln -svf $HOME/dotfiles/topics/bash/bashrc $HOME/.bashrc
+}
+
+bitwarden() {
+	yay -S --noconfirm --sudoloop bitwarden
+}
+
+bluetooth() {
+	yay -S --noconfirm --sudoloop --needed blueberry blueman bluez-hid2hci bluez-tools bluez-utils python-pybluez
+}
+bspwm() {
+	yay --noconfirm --sudoloop --needed bspwm-rounded-corners-git bsp-layout sxhkd-git bar-aint-recursive xtitle-git sutils-git
+	ln -svf $HOME/dotfiles/topics/bspwm $HOME/.config/bspwm
+
+}
+chromium() {
+	sudo pacman -S --noconfirm chromium
+}
+
+cups() {
+	yay -S --noconfirm --sudoloop --batchinstall cups cups-filters cups-pdf cups-pk-helper foomatic-db foomatic-db-engine foomatic-db-gutenprint-ppds cups cups-filters cups-pdf cndrvcups-lb-bin cnijfilter2-bin cups-pk-helper
+}
+disks() {
+
+	yay -S --noconfirm --sudoloop --needed dosfstools findutils gzip lzip p7zip tar
+	yay -S --noconfirm --sudoloop --needed bzip2 lzop udftools cpio file file-roller filesystem
+	yay -S --noconfirm --sudoloop --needed diskus xz e2fsprogs diskus coreutils dosfstools exfat-utils fatresize
+	yay -S --noconfirm --sudoloop --needed gvfs gvfs-afc gvfs-goa gvfs-google lostfiles lzop
+	yay -S --noconfirm --sudoloop --needed gvfs-gphoto2 gvfs-mtp gvfs-nfs lrzip hardinfo hddtemp bzip2
+	yay -S --noconfirm --sudoloop --needed gvfs-smb xarchiver unrar unarj unzip borg btrfs-progs
+}
+docker() {
+	yay -S --noconfirm --sudoloop --needed docker docker-compose docker-machine python-docker containerd
+	sudo usermod -aG docker ${USER}
+	sudo systemctl enable --now docker.service
+	sudo systemctl enable --now docker.socket
+
+}
+
+dunst() {
+	ln -svf $HOME/dotfiles/topics/dunst/dunstrc $HOME/.config/dunst/dunstrc
+}
+
+####################################################################################
+## Devices #########################################################################
+####################################################################################
+
+laptop() {
+	amd
+	awesomewm
+	bash
+	bitwarden
+	bluetooth
+	bspwm
+	chromium
+	cups
+	disks
+	docker
+	dunst
 	sh $HOME/dotfiles/topics/font/install.sh
 	sh $HOME/dotfiles/topics/gparted/install.sh
 	sh $HOME/dotfiles/topics/gimp/install.sh
@@ -89,18 +175,18 @@ laptop() {
 }
 
 workstation() {
+	amd
+	awesomewm
+	bash
+	bitwarden
+	bluetooth
+	bspwm
+	chromium
+	cups
 
-	sh $HOME/dotfiles/topics/amd/install.sh
-	sh $HOME/dotfiles/topics/awesome/install.sh
-	sh $HOME/dotfiles/topics/bash/install.sh
-	sh $HOME/dotfiles/topics/bitwarden/install.sh
-	sh $HOME/dotfiles/topics/bluetooth/install.sh
-	sh $HOME/dotfiles/topics/bspwm/install.sh
-	sh $HOME/dotfiles/topics/chromium/install.sh
-	sh $HOME/dotfiles/topics/cups/install.sh
-	sh $HOME/dotfiles/topics/cursor/install.sh
-	sh $HOME/dotfiles/topics/disks/install.sh
-	sh $HOME/dotfiles/topics/docker/install.sh
+	disks
+	docker
+	dunst
 	sh $HOME/dotfiles/topics/font/install.sh
 	sh $HOME/dotfiles/topics/gparted/install.sh
 	sh $HOME/dotfiles/topics/gimp/install.sh
@@ -145,13 +231,13 @@ workstation() {
 
 rpi4() {
 
-	sh $HOME/dotfiles/topics/awesome/install.sh
-	sh $HOME/dotfiles/topics/bash/install.sh
-	sh $HOME/dotfiles/topics/bitwarden/install.sh
-	sh $HOME/dotfiles/topics/bluetooth/install.sh
-	sh $HOME/dotfiles/topics/cursor/install.sh
-	sh $HOME/dotfiles/topics/disks/install.sh
-	sh $HOME/dotfiles/topics/docker/install.sh
+	awesomewm
+	bash
+	bitwarden
+	bluetooth
+	disks
+	docker
+	dunst
 	sh $HOME/dotfiles/topics/font/install.sh
 	sh $HOME/dotfiles/topics/git/install.sh
 	sh $HOME/dotfiles/topics/gtk/install.sh
@@ -185,21 +271,35 @@ rpi4() {
 }
 
 menu() {
-	echo " Select the device to provision from the list below."
+	clear
+
+	echo -n "
+  _____         __    ___ __ __                   
+ |     \.-----.|  |_.'  _|__|  |.-----.-----.     
+ |  --  |  _  ||   _|   _|  |  ||  -__|__ --|     
+ |_____/|_____||____|__| |__|__||_____|_____|     
+  __               __          __ __              
+ |__|.-----.-----.|  |_.---.-.|  |  |.-----.----. 
+ |  ||     |__ --||   _|  _  ||  |  ||  -__|   _| 
+ |__||__|__|_____||____|___._||__|__||_____|__|   "
+
+	echo
+	echo
+	echo
+	echo " Select the device to provision from the list below." \n\n\n
 	echo "-------------------------------------------------------"
 	echo "                1 ) Workstation      "
 	echo "                2 ) Laptop           "
 	echo "                3 ) Raspberry Pi 4   "
 	echo "-------------------------------------------------------"
 	read system
-	echo -n ""
-	echo -n ""
+	echo
+	echo
+	echo
 	case $system in
 	1) # Workstation Configuration
 		echo
 		echo "You have chosen the workstation provisioning and configuration option."
-		echo
-		echo
 		workstation
 		echo
 		echo
--- topics/cups/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env bash
-
-##############################################################################
-## CUPS Printer Drivers ######################################################
-##############################################################################
-
-yay -S --noconfirm --sudoloop --batchinstall cups cups-filters cups-pdf cups-pk-helper foomatic-db foomatic-db-engine foomatic-db-gutenprint-ppds cups cups-filters cups-pdf cndrvcups-lb-bin cnijfilter2-bin cups-pk-helper
