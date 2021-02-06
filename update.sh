bc98b6878e78e0db358d3020cec24cda3723b4d9
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-02-06 00:28:12 -0800

    dotfiles group update

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/env bash
 
-    cd ${HOME}/.config/awesome && git add . && git commit -m "awesome window manager config has been updated" && git push --force
+    cd ${HOME}/.config/awesome && git add . && git commit -m "awesome updated" && git push --force
    
-    cd ${HOME}/.config/qtile && git add . && git commit -m "qtile configuration has been updated" && git push --force
+    cd ${HOME}/.config/qtile && git add . && git commit -m "qtile updated" && git push --force
    
-    cd ${HOME}/.local/share/bin && git add . && git commit -m "my bin scripts have been updated" && git push --force
+    cd ${HOME}/.local/share/bin && git add . && git commit -m "bin scripts updated" && git push --force
 
-    cd ${HOME}/vimwiki && git add . && git commit -m "vimwiki is updated" && git push --force
+    cd ${HOME}/.vimwiki-tech && git add . && git commit -m "vimwiki updates" && git push --force
 
     cd ${HOME}/dotfiles && git add . && git commit -m "dotfiles group update" && git push --force
