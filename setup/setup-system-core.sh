39449f90eb919f32501dcdeddce2240f0f523dce
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-11-29 05:55:26 -0800

    make things work a bit better

--- setup/setup-system-core.sh
+++ setup/setup-system-core.sh
@@ -52,7 +52,7 @@ sleep 3s
 	bypass
 clear
 
-echo "Updating system and adding repositories..."
+print s "Updating system and adding repositories..."
 sleep 3s
 	sudo xbps-install -Syu
 	sudo xbps-install -Sy void-repo-nonfree
@@ -61,7 +61,7 @@ sleep 3s
 	sudo xbps-install -Sy void-repo-debug
 clear
 
-echo "Installing packages..."
+print s "Installing packages..."
 sleep 3s
 #services [stock installed - dhcpcd udevd]
 	sudo xbps-install -Syv dbus dbus-elogind elogind polkit
@@ -72,7 +72,7 @@ sleep 3s
 #cpu and gpu firmware modules
 	sudo xbps-install -Syv mesa MesaLib MesaLib-devel glu glu-devel libgbm libgbm-devel mesa-dri mesa-ati-dri mesa mesa-dbg   mesa-opencl amdvlk xf86-video-amdgpu linux-firmware-amd 
 		
-#de - sway
+#de/wm
 	sudo xbps-install -Syv awesome rofi light kitty rofi-devel 
 		
 		#autotling
@@ -81,25 +81,23 @@ sleep 3s
 #network
 	sudo xbps-install -Syv NetworkManager 
 	#sudo xbps-install -Syv connman
-	#sudo xbps-install -Syv linux-firmware-network #FREE, easy to use implementation of the Network Time Protocol
+	sudo xbps-install -Syv linux-firmware-network #FREE, easy to use implementation of the Network Time Protocol
 		
 		#wireguard #https://www.wireguard.com/install/
 			#sudo xbps-install -Syv wireguard-tools wireguard-dkms
 
-		#bemnu: https://github.com/Cloudef/bemenu	https://github.com/firecat53/networkmanager-dmenu
+	
 		sudo xbps-install -Syv networkmanager-dmenu
 
 #bluetooth
 	sudo xbps-install -Syv bluez
 	sudo xbps-install -Syv libspa-bluetooth #Server and user space API to deal with multimedia pipelines - bluetooth plugins
-	#sudo xbps-install -Syv sbc #Bluetooth Subband Codec (SBC) library
-	sudo xbps-install -Syv broadcom-bt-firmware
-	#libbluetooth #Library to use the Bluez Linux Bluetooth Stack
-	#hidapi #Simple library for communicating with USB and Bluetooth HID devices
-	#blueman #GTK+ Bluetooth Manager
-	#blueberry
-	#bluez-alsa #Bluetooth Audio ALSA Backend
-	#gnome-bluetooth #GNOME Bluetooth Subsystem
+	sudo xbps-install -Syv sbc #Bluetooth Subband Codec (SBC) library
+	sudo xbps-install -Syv broadcom-bt-firmware libbluetooth #Library to use the Bluez Linux Bluetooth Stack
+	sudo xbps-install -Syv hidapi #Simple library for communicating with USB and Bluetooth HID devices
+	sudo xbps-install -Syv blueman #GTK+ Bluetooth Manager
+	sudo xbps-install -Syv bluez-alsa #Bluetooth Audio ALSA Backend
+	
 
 #audio
 	sudo xbps-install -Syv alsa-utils pulseaudio alsa-plugins-pulseaudio pamixer playerctl ncpamixer
@@ -123,23 +121,19 @@ sleep 3s
 #passwords
 	sudo xbps-install -Syv keepassxc
 
-	#https://www.passwordstore.org/
-	#sudo xbps-install -Syv pass qtpass
+
 
 #internet
 	sudo xbps-install -Sy dhcpcd-gtk wpa_gui
 	sudo xbps-install -Syv firefox elinks
 	sudo xbps-install -Syv transmission-gtk
-	#https://github.com/vladimiry/ElectronMail/releases
-		#chat
-	#sudo xbps-install -Syv fractal #hexchat #dino
+
 		
 		#privacy
 			sudo xbps-install -Syv ufw  gufw 
 			sudo xbps-install -Syv dnscrypt-proxy
 
 #system utilities
-	sudo xbps-install -Syv gedit
 	#sudo xbps-install -Syv numlockx
 	sudo xbps-install -Syv rsync	
 	sudo xbps-install -Syv gnome-disk-utility
@@ -147,7 +141,6 @@ sleep 3s
 	sudo xbps-install -Syv autofs # automount usb
 	sudo xbps-install -Syv xeyes # show X11 or Wayland
 	sudo xbps-install -Syv xev # show key press
-	sudo xbps-install -Syv gnome-boxes
 	sudo xbps-install -Syv dconf-editor
 	sudo xbps-install -Syv ntfs-3g #Stable Read/Write NTFS Driver in userspace
 
@@ -155,9 +148,6 @@ sleep 3s
 	sudo xbps-install -Syv neofetch wget git gotop tty-clock calcurse vitetris
 	sudo xbps-install -Syv bash-completion
 	sudo xbps-install -yv ranger
-	#sudo xbps-install -yv lf # Terminal file manager - written in go https://github.com/gokcehan/lf
-	#sudo xbps-install -Syv elvish
-	#sudo xbps-install -yv vifm # Ncurses-based file manager with vi-like keybindings
 
 #android
 	sudo xbps-install -Syv android-tools
@@ -165,30 +155,12 @@ sleep 3s
 #nvidia - https://docs.voidlinux.org/config/graphics/optimus.html
 	#sudo xbps-install -Syv nvidia glxinfo
 
-#gaming
-	#sudo xbps-install -Syv lutris steam piper #GTK application to configure gaming mice
-		#https://wiki.voidlinux.org/Wine
-		#sudo xbps-install -Syv  wine-32bit
-		#https://wiki.voidlinux.org/Steam
-		#sudo xbps-install -Syv libpulseaudio-32bit libtxc_dxtn-32bit fontconfig-32bit libavcodec-32bit libavformat-32bit libavresample-32bit libavutil-32bit
-		#sudo xbps-install -Syv libGL-32bit mesa-intel-dri-32bit
-		#sudo xbps-install -Syv libGL-32bit mesa-nouveau-dri-32bit
-		#sudo xbps-install -Syv nvidia-libs-32bit
-		#sudo xbps-install -Syv mono
 
 #unsure
 	sudo xbps-install -Syv openntpd openresolv
 
 #development
 	sudo xbps-install -Syv curl ninja make
-
-#gnome
-	#sudo xbps-install -Syv gnome-contacts
-	#sudo xbps-install -Syv gnome-calculator
-	#sudo xbps-install -Syv gnome-terminal
-	#sudo xbps-install -Syv gnome-font-viewer
-	#sudo xbps-install -Syv gnome-latex
-	#sudo xbps-install -Syv gnome-bluetooth
 
 #flatpak
 	sudo xbps-install -Syv flatpak
@@ -204,7 +176,7 @@ sleep 3s
 clear
 
 
-echo "Installing new theme..."
+print s "Installing new theme..."
 sleep 3s
 #	/usr/local/bin/void-scripts/plata-custom-void.sh
 #	/usr/local/bin/void-scripts/papirus-custom-void.sh
@@ -212,7 +184,7 @@ sleep 3s
 
 clear
 
-echo "Configuring system..."
+print s "Configuring system..."
 sleep 3s
 
 	sudo ln -s /etc/sv/dbus /var/service/
@@ -252,7 +224,7 @@ sleep 3s
 
 # fix swap
 	sudo mkdir /etc/sysctl.d/
-	echo 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-swappiness.conf
+	print s 'vm.swappiness=10' | sudo tee /etc/sysctl.d/99-swappiness.conf
 
 # fix text
 	sudo ln -s /usr/share/fontconfig/conf.avail/10-hinting-full.conf /etc/fonts/conf.d/
@@ -269,7 +241,7 @@ clear
 	#	setleds -D +num < $tty > /dev/null
 	#done
 
-echo "Adding user to some groups..."
+print s "Adding user to some groups..."
 sleep 3s
 	sudo usermod -a -G input $USER
 	sudo usermod -a -G audio $USER
@@ -283,25 +255,17 @@ sleep 3s
 	sudo usermod -aG socklog $USER
 clear
 
-read -p "Done! Press ENTER to reboot."
-	sudo shutdown -r now
-   print s "###############################################################################"
-    print s "Completed Package Installation"
-    print s "###############################################################################"
-}
+
 
 ## First Setup Needed Directories
 mkdir -p ~/.config
 mkdir -p ~/.local/share/bin
-
+}
 clonePackages() {
     print s "###############################################################################"
     print s "Git Cloning of Out-of-Repo Packages"
     print s "###############################################################################"
-    # VPSM
-
-    git clone git@github.com:the-Electric-Tantra-Linux/void-packages.git ~/.void-packages
-
+  
     # XDEB
     git clone https://github.com/toluschr/xdeb $HOME/xdeb && cd $HOME/xdeb && sudo chmod 744 xdeb/xdeb && sudo mv xdeb/xdeb /usr/local/bin/ && rm xdeb
 
@@ -318,8 +282,8 @@ clonePackages() {
     sudo mkdir -p /glibc
     sudo XBPS_ARCH=x86_64 xbps-install --repository=http://alpha.de.repo.voidlinux.org/current -r /glibc -S base-voidstrap
     git clone https://github.com/gch1p/voidnsrun $HOME/voidnsrun && cd voidnsrun && make run && sudo make install-run && cd $HOME && rm voidnsrun
-    echo "export VOIDNSRUN_DIR=/glibc" >>.zshrc
-    echo "export VOIDNSUNDO_BIN=/usr/local/bin/voidnsundo" >>~/.zshrc
+    print s "export VOIDNSRUN_DIR=/glibc" >>.zshrc
+    print s "export VOIDNSUNDO_BIN=/usr/local/bin/voidnsundo" >>~/.zshrc
     print s "###############################################################################"
     print s "Completed Git Cloning of Out-of-Repo Packages"
     print s "###############################################################################"
