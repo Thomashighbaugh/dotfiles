7ea56bede792bbc93289ffec5becf48c78ad33ff
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2022-02-25 23:02:08 -0800

    stuff

---
+++ home/tym/config.lua
@@ -0,0 +1,19 @@
+-- At first, you need to require tym module
+local tym = require('tym')
+
+-- set individually
+--tym.set('width', 100)
+
+
+-- set by table
+tym.set_config({
+  shell = '/usr/bin/zsh',
+  cursor_shape = 'underline',
+  autohide = true,
+  color_foreground = '#8b9cbe',
+  silent = true,
+  scrollback_length=1024,
+  padding_vertical=12,
+  padding_horizontal=12,
+  cursor_blink_mode= 'on'
+})
