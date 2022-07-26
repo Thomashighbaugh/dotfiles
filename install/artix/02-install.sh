9865ed7cd073dff301b99b32361f079caab0b5ba
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2022-07-26 08:22:44 -0700

    bork

--- install/artix/02-install.sh
+++ install/artix/02-install.sh
@@ -7,12 +7,12 @@ print s "[===================================================]"
 print s Install Paru
 print s "[===================================================]"
 
-sudo pacman -S --noconfirm --needed git base-devel fakeroot
+sudo pacman -S --noconfirm --needed git base-devel fakeroot ccache
 
 if ! command -v paru &>/dev/null; then
-    sudo rm -rvf /tmp/paru-bin-cloned
-    git clone https://aur.archlinux.org/paru.git /tmp/paru
-    cd /tmp/paru-bin-cloned/ && makepkg -si
+    sudo rm -rvf /tmp/paru
+    git clone https://aur.archlinux.org/paru-bin.git /tmp/paru
+    cd /tmp/paru && makepkg -si
 fi
 
 # --------------------------------------------------- #
@@ -21,12 +21,13 @@ print s Pacman Configuration
 print s "[===================================================]"
 
 sudo pacman-key --init
-sudo pacman-key --populate archlinux
 #------------------------#
 # Swap in Our pacman.conf
 
 if [[ -z "$PACMAN" ]]; then
-
+    print s "[===================================================]"
+    print s Add Universe and Multiverse Repos 
+    print s "[===================================================]"
     sudo bash -c "echo '[universe]' >> /etc/pacman.conf"
     sudo bash -c "echo 'Server = https://universe.artixlinux.org/$arch' >> /etc/pacman.conf"
     sudo bash -c "echo 'Server = https://mirror1.artixlinux.org/universe/$arch' >> /etc/pacman.conf"
@@ -37,17 +38,13 @@ if [[ -z "$PACMAN" ]]; then
     sudo bash -c "echo '[omniverse]' >> /etc/pacman.conf"
     sudo bash -c "echo 'Server = http://omniverse.artixlinux.org/$arch' >> /etc/pacman.conf"
     sudo pacman -Syyu
-    sudo pacman -S artix-archlinux-support
-    sudo pacman-key --populate archlinux
-    sudo bash -c "echo '[extra]' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Include = /etc/pacman.d/mirrorlist-arch' >> /etc/pacman.conf"
-    sudo bash -c "echo '[community]' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Include = /etc/pacman.d/mirrorlist-arch' >> /etc/pacman.conf"
-    sudo pacman -Syyu
     export PACMAN=0
 
     #----------------------------------#
     # Add Choatic AUR for kernel builds
+     print s "[===================================================]" 
+     print s Chaos AUR Repository 
+     print s "[===================================================]"
     curl https://aur.chaotic.cx/chaotic.gpg >/tmp/chaotic.pgp
     sudo pacman-key --add /tmp/chaotic.pgp
     sudo pacman-key --lsign-key FBA220DFC880C036
@@ -58,6 +55,9 @@ if [[ -z "$PACMAN" ]]; then
 
     #--------------------------------#
     # Add Blackarch for Security Tools
+     print s "[===================================================]" 
+     print s BlackArch Repos 
+     print s "[===================================================]"
     cd /tmp && curl -O https://blackarch.org/strap.sh && sudo chmod +x strap.sh && sudo ./strap.sh && sudo pacman -Syu --noconfirm && cd $HOME/dotfiles || return
 
 else
@@ -75,6 +75,7 @@ print s "[===================================================]"
 numberofcores=$(grep -c processor /proc/cpuinfo)
 if [ $numberofcores -gt 1 ]; then
     sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores + 1))'"/g' /etc/makepkg.conf | tee -a /tmp/install-log.txt
+    sudo sed -i 's/!ccache/ccache/g' /etc/makepkg.conf | tee -a /tmp/install-log.txt
     sudo sed -i 's/xz -c -z -/xz -c -z - --threads=0/g' /etc/makepkg.conf | tee -a /tmp/install-log.txt
     sudo sed -i 's/zstd -c -z -q -/zstd -c -z -q - --threads=0/g' /etc/makepkg.conf | tee -a /tmp/install-log.txt
     sudo sed -i "s/PKGEXT='.pkg.tar.xz'/PKGEXT='.pkg.tar.zst'/g" /etc/makepkg.conf | tee -a /tmp/install-log.txt
@@ -83,7 +84,7 @@ else
 fi
 # --------------------------------------------------- #
 print s "[===================================================]"
-print s Install Packages | tee -a /tmp/install-log.txt
+print s Install Packages
 print s "[===================================================]"
 
 for line in $(cat $HOME/dotfiles/install/artix/pkglist); do
@@ -92,7 +93,7 @@ done
 
 # --------------------------------------------------- #
 print s "[===================================================]"
-print s Install Luarocks Packages | tee -a /tmp/install-log.txt
+print s Install Luarocks Packages 
 print s "[===================================================]"
 
 paru -S --needed --noconfirm luarocks
@@ -106,7 +107,7 @@ sudo pacman -Rncs --noconfirm xfce4-screensaver xfce4-screenshooter xfce4-power-
 
 # --------------------------------------------------- #
 print s "[===================================================]"
-print s "Install Out-of-Repo Packages" | tee -a /tmp/install-log.txt
+print s "Install Out-of-Repo Packages" 
 print s "[===================================================]"
 
 git clone https://github.com/khuedoan/slock /tmp/slock && cd /tmp/slock && sudo make clean install && $HOME/dotfiles
