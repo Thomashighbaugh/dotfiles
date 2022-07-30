c0a8db0946ffe0415ee98c872b88e0f095da94c0
Thomashighbaugh <thighbaugh@zoho.com>
2022-07-29 23:07:43 -0700

    youtube-dl to yt-dlp

--- home/shell/alias/youtube.alias.sh
+++ home/shell/alias/youtube.alias.sh
@@ -6,11 +6,11 @@
 #### without the express permission of the copyright owner
 
 # Download Audio Track only
-alias yt-audio="youtube-dl -x  --audio-format mp3 --audio-quality 0"
+alias yt-audio="yt-dlp -x  --audio-format mp3 --audio-quality 0"
 
 # Download YouTube playlist videos in separate directory indexed by video order in a playlist
-alias yt-dl-playlist="youtube-dl -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'"
+alias yt-dl-playlist="yt-dlp -o '%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'"
 
 # Download all playlists of YouTube channel/user keeping each playlist in separate directory
-alias yt-dl-user="youtube-dl -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'  "
-alias yadl-user="youtube-dl -x  --audio-format mp3 --audio-quality 0 -o  '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'  "
+alias yt-dl-user="yt-dlp -o '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'  "
+alias yadl-user="yt-dlp -x  --audio-format mp3 --audio-quality 0 -o  '%(uploader)s/%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'  "
--- install/artix/02-install.sh
+++ install/artix/02-install.sh
@@ -24,45 +24,18 @@ sudo pacman-key --init
 #------------------------#
 # Swap in Our pacman.conf
 
-if [[ -z "$PACMAN" ]]; then
-    print s "[===================================================]"
-    print s Add Universe and Multiverse Repos 
-    print s "[===================================================]"
-    sudo bash -c "echo '[universe]' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://universe.artixlinux.org/$arch' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://mirror1.artixlinux.org/universe/$arch' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://mirror.pascalpuffke.de/artix-universe/$arch' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://artixlinux.qontinuum.space/artixlinux/universe/os/$arch' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://mirror1.cl.netactuate.com/artix/universe/$arch' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = https://ftp.crifo.org/artix-universe/' >> /etc/pacman.conf"
-    sudo bash -c "echo '[omniverse]' >> /etc/pacman.conf"
-    sudo bash -c "echo 'Server = http://omniverse.artixlinux.org/$arch' >> /etc/pacman.conf"
-    sudo pacman -Syyu
-    export PACMAN=0
-
-    #----------------------------------#
-    # Add Choatic AUR for kernel builds
-     print s "[===================================================]" 
-     print s Chaos AUR Repository 
-     print s "[===================================================]"
-    curl https://aur.chaotic.cx/chaotic.gpg >/tmp/chaotic.pgp
-    sudo pacman-key --add /tmp/chaotic.pgp
-    sudo pacman-key --lsign-key FBA220DFC880C036
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-    sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf '
-    sudo sh -c 'echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf'
-    sudo pacman -Syy
-
+if [[ -z "/tmp/pacman" ]]; then
+    sudo cp -rvf $HOME/dotfiles/root/pacman/artix/pacman.conf /etc/pacman.conf
     #--------------------------------#
     # Add Blackarch for Security Tools
      print s "[===================================================]" 
      print s BlackArch Repos 
      print s "[===================================================]"
     cd /tmp && curl -O https://blackarch.org/strap.sh && sudo chmod +x strap.sh && sudo ./strap.sh && sudo pacman -Syu --noconfirm && cd $HOME/dotfiles || return
-
+sudo touch /tmp/pacman
 else
 
-    return
+sleep 3
 
 fi
 
