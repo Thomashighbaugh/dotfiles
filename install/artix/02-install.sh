a8021ef600501f56bacff5b1ea60065375411d83
Thomashighbaugh <thighbaugh@zoho.com>
2022-12-04 05:42:21 -0800

    wget\

--- install/artix/02-install.sh
+++ install/artix/02-install.sh
@@ -38,6 +38,12 @@ sudo pacman-key --populate archlinux
 
 sudo pacman -Syyu
 
+# Blackarch Repo
+curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && sudo bash strap.sh && sudo pacman -Syu
+
+# Chaotic-AUR 
+
+sudo bash -c "pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com&& pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && echo '[chaotic-aur]' >> /etc/pacman.conf && echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
 
 
 
