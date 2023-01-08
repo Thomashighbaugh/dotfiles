58cde4eb0a28856be188c7bf301677dbd9c4114f
Thomashighbaugh <thighbaugh@zoho.com>
2023-01-08 15:09:17 +0000

    remove strap

--- install/artix/02-install.sh
+++ install/artix/02-install.sh
@@ -2,24 +2,24 @@
 
 source $HOME/dotfiles/install/artix/01-lib.sh
 
-
 # --------------------------------------------------- #
 
-print s "[===================================================]"
-print s "Enabling Arch Repositories..." 
-print s "[===================================================]"
-
+print t "[===================================================]"
+print t "Enabling Artix Repositories..."
+print t "[===================================================]"
+# --------------------------------------------------- #
 sudo pacman -S --noconfirm artix-mirrorlist git base-devel fakeroot ccache
+# --------------------------------------------------- #
+print s "[===================================================]"
+print s "Enabling Arch Repositories..."
+print s "[===================================================]"
 
 sudo cp -rvf root/pacman/artix/pacman.conf /etc/
-
 sudo pacman-key --init
-sudo pacman-key --populate artix 
-
-sudo pacman -Syy 
-
-sudo pacman --noconfirm -Sy --overwrite='*' artix-keyring artix-archlinux-support archlinux-mirrorlist 
-
+sudo pacman-key --populate artix
+sudo pacman-key --refresh-keys
+sudo pacman -Syy
+sudo pacman --noconfirm -Sy --overwrite='*' artix-keyring artix-archlinux-support archlinux-mirrorlist
 
 sudo bash -c 'echo " # Extra
 # Arch Linux Repositories 
@@ -31,22 +31,27 @@ sudo bash -c 'echo " # Extra
  Include = /etc/pacman.d/mirrorlist-arch
 
  [multilib]
- Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf' 
+ Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf'
 
-
-sudo pacman-key --populate archlinux
-
+sudo pacman-key --populate archlinux artix
 sudo pacman -Syyu --overwrite='*'
 
-# Blackarch Repo
+# --------------------------------------------------- #
+print s "[===================================================]"
+print s "Enabling BlackArch Repositories..."
+print s "[===================================================]"
 curl -O https://blackarch.org/strap.sh && chmod +x strap.sh && sudo bash strap.sh && sudo pacman -Syu
 
-# Chaotic-AUR 
-
+# --------------------------------------------------- #
+print s "[===================================================]"
+print s "Enabling Chaotic AUR Repositories..."
+print s "[===================================================]"
 sudo bash -c "pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com&& pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' && echo '[chaotic-aur]' >> /etc/pacman.conf && echo 'Include = /etc/pacman.d/chaotic-mirrorlist' >> /etc/pacman.conf"
-
 sudo pacman -Syy
-
+# --------------------------------------------------- #
+print t "[===================================================]"
+print t "Enabling and Optimizing AUR Package Management..."
+print t "[===================================================]"
 # --------------------------------------------------- #
 print s "[===================================================]"
 print s Install Paru
@@ -57,14 +62,10 @@ if ! command -v paru &>/dev/null; then
     git clone https://aur.archlinux.org/paru-bin.git /tmp/paru
     cd /tmp/paru && makepkg -si
 fi
-
-
 # --------------------------------------------------- #
 print s "[===================================================]"
 print s Optimize Makepkg
 print s "[===================================================]"
-
-# makepkg.conf optimization
 numberofcores=$(grep -c processor /proc/cpuinfo)
 if [ $numberofcores -gt 1 ]; then
     sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores + 1))'"/g' /etc/makepkg.conf | tee -a /tmp/install-log.txt
@@ -76,24 +77,29 @@ else
     echo "makepkg.conf not changed." | tee -a /tmp/install-log.txt
 fi
 # --------------------------------------------------- #
-print s "[===================================================]"
-print s Install Packages
-print s "[===================================================]"
+print t "[===================================================]"
+print t Install Packages
+print t "[===================================================]"
 
 for line in $(cat $HOME/dotfiles/install/artix/pkglist); do
     paru -S --noconfirm --needed --overwrite='*' $line | tee -a /tmp/install-log.txt
 done
 
-# freetype2 
-paru -Rns freetype2 
-paru -S --noconfirm --needed --overwrite='*' freetype2-infinality-remix 
-
-# For Thunar 
+# For Thunar Extensions
 uca-apply update
 
 # --------------------------------------------------- #
+print t "[===================================================]"
+print t Remove Packages
+print t "[===================================================]"
+
+# Remove
+# Get rid of these pains now, even if xfce4 is a useful secondary DE
+sudo pacman -Rncs --noconfirm xfce4-screensaver xfce4-screenshooter xfce4-power-manager
+
+# --------------------------------------------------- #
 print s "[===================================================]"
-print s Install Luarocks Packages 
+print s Install Luarocks Packages
 print s "[===================================================]"
 
 paru -S --needed --noconfirm --overwrite='*' luarocks
@@ -102,12 +108,4 @@ for package in "luasec" "luaposix" "luacheck" "luafilesystem" "ldoc" "lpeg" "arg
     sudo luarocks install $package | tee -a /tmp/install-log.txt
 done
 
-# Get rid of these pains now, even if xfce4 is a useful secondary DE
-sudo pacman -Rncs --noconfirm xfce4-screensaver xfce4-screenshooter xfce4-power-manager
-
 # --------------------------------------------------- #
-print s "[===================================================]"
-print s "Install Out-of-Repo Packages" 
-print s "[===================================================]"
-
-git clone https://github.com/khuedoan/slock /tmp/slock && cd /tmp/slock && sudo make clean install && $HOME/dotfiles
